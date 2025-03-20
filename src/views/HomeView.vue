a88a408e6b41e3dc7d1f7769609146e13dd21ee6
= <=>
2025-03-20 16:15:22 +0100

    chore: Remove unused components and update navigation styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <img src="@/assets/logo.svg" alt="Logo" style="width: 25px;">
+    <a class="navbar-brand" href="#">Examen UF2</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav gap-3">
+        <li class="nav-item btn btn-outline-primary">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item btn btn-outline-primary">
+          <a class="nav-link" href="#">Aigua</a>
+        </li>
+        <li class="nav-item btn btn-outline-primary">
+          <a class="nav-link" href="#">Llista de la compra</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
 </template>
+<style scoped>
+
+</style>
